e9c28bf9cde9c406e4d967f4e69f9c79c479bde4
Szymon Małolepszy <szymon.malolepszy@softwareplant.com>
2022-10-28 16:15:25 +0200

    Some padding and rounded counters

--- src/app/game/player-panel/player-panel.component.ts
+++ src/app/game/player-panel/player-panel.component.ts
@@ -1,11 +1,13 @@
-import { Component, ElementRef, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { ATTRIBUTE_INDEXES, ZERO_ARRAY_FOR_ATTRIBUTES } from 'src/app/game/player-panel/player-attributes';
 import { PlayerVisualization } from 'src/app/game/player-panel/PlayerVisualization';
+import { RotationDirection } from 'src/app/game/directives/RotationDirection';
 
 @Component({
   selector: 'app-player-panel',
   templateUrl: './player-panel.component.html',
-  styleUrls: ['./player-panel.component.css']
+  styleUrls: ['./player-panel.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PlayerPanelComponent implements OnInit {
 
@@ -17,17 +19,21 @@ export class PlayerPanelComponent implements OnInit {
 
   constructor(
     public el: ElementRef
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.attributeValues = [...ZERO_ARRAY_FOR_ATTRIBUTES];
   }
 
-  increaseValue(index: number) {
+  increaseValue(index: number): void {
     this.attributeValues[index]++;
   }
 
-  decreaseValue(index: number) {
+  decreaseValue(index: number): void {
     this.attributeValues[index] = Math.max(--this.attributeValues[index], 0);
+  }
+
+  isRotated(orientation: RotationDirection): boolean {
+    return orientation === RotationDirection.RIGHT || orientation === RotationDirection.LEFT;
   }
 }
